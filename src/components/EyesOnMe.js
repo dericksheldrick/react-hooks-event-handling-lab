0f00dcfab32ccc68142e1e391a8ca0b4193f3fd2
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-04-17 17:33:43 +0300

    event submission

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+    // function handleClick(event){
+    //     console.log("He clicked on me")
+    // }
+
+    function handleBlur(event){
+        console.log("Hey! Eyes on me")
+
+    }
+    function handleFocus(event){
+        console.log("Good!")
+    }
+  return (
+    <div>
+        <button /*onClick={handleClick}*/ onFocus={handleFocus} onBlur={handleBlur}>Eyes On Me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" placeholder="Enter Password" onChange={handleChange} name="password" />
+        </div>
     )
 }
 
